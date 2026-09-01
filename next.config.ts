22d00e09986478034c93f5a65e295305d812f34f
balikumarwad <balikummarwad@gmail.com>
2026-09-01 23:26:24 +0545

    next.config.ts updated

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  output: 'export', // <-- Add this line
+const nextConfig: NextConfig = {
   images: {
-    unoptimized: true, // <-- Highly recommended for GitHub Pages
+    unoptimized: true,
   },
+  // Ensure basePath is completely left out or empty if using a root domain
 };
 
-module.exports = nextConfig;
+export default nextConfig;
